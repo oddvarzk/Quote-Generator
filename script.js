65d21018a1640b06334f2170bbfbcf6f8d7af335
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-05-25 07:19:15 +0200

    Tweet Quote Btn finished, event listeners added to both buttons for functionality

--- script.js
+++ script.js
@@ -40,10 +40,14 @@ async function getQuotes() {
 }
 
 //Tweet Quote
-function tweetQuote(){
-    const twitterUrl =
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
 }
 
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 //On Load
 getQuotes();
