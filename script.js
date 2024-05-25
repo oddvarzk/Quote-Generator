8af206d6316fd62f2994ba583fd92c56c625562f
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-05-25 07:37:39 +0200

    Loader added to showup when API is fetching quote

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // Select all HTML boxes by ID to populate here:
-const quoteContainer = document.getElementById("quote-generator");
+const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+//Show function
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new Quote
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if Author field is blank, if yes replace with "Unknown" string.
@@ -23,12 +38,15 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  // Set Quote, Hide Loader
 
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes from an API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
