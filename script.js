040e912d6152a7b2692067c5393697cefeb5511d
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-05-25 02:36:55 +0200

    API Call for Quotes added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+let apiQuotes = [];
+
+// Get Quotes from an API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    console.log(apiQuotes[12]);
+  } catch (error) {
+    //Catch any error here
+  }
+}
+//On Load
+getQuotes();
